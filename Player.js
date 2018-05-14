fb7617ca1639b9d4e8e57527a5093eb0492806e4
Adam Grob <ad4mgr0b@gmail.com>
2018-05-14 11:57:59 +0200

    bugfix

--- Player.js
+++ Player.js
@@ -6,13 +6,13 @@ class Player {
   static betRequest(gameState, bet) {
     let cards = Parser.fullhand(gameState);
 
-    if (HandEvaluator.isThreeOfAKind(cards)){
+    // if (HandEvaluator.isThreeOfAKind(cards)){
       bet(1000);
-    } else if (HandEvaluator.isPair(cards)) {
-      bet(100);
-    } else {
-      bet(Parser.to_call(gameState));
-    }
+    // } else if (HandEvaluator.isPair(cards)) {
+    //   bet(100);
+    // } else {
+    //   bet(Parser.to_call(gameState));
+    // }
   }
 
   static showdown(gameState) {
